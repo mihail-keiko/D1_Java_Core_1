a4b3e67c4cb60d4594a8d8cb7f3ccba87038dc65
Mihail Keiko <Mikhail_Keiko@epam.com>
2019-09-13 14:11:32 +0300

    add property

--- src/com/epam/aircompany/model/AbstractFreightTransport.java
+++ src/com/epam/aircompany/model/AbstractFreightTransport.java
@@ -1,4 +1,13 @@
 package com.epam.aircompany.model;
 
 public class AbstractFreightTransport extends AbstractAviaTechnique {
+    private Double carryingCapacity;
+
+    public Double getCarryingCapacity() {
+        return carryingCapacity;
+    }
+
+    public void setCarryingCapacity(Double carryingCapacity) {
+        this.carryingCapacity = carryingCapacity;
+    }
 }
--- src/com/epam/aircompany/model/AbstractPassengerTransport.java
+++ src/com/epam/aircompany/model/AbstractPassengerTransport.java
@@ -1,4 +1,13 @@
 package com.epam.aircompany.model;
 
 public class AbstractPassengerTransport extends AbstractAviaTechnique {
+    private Integer numberOfSeats;
+
+    public Integer getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(Integer numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
 }
